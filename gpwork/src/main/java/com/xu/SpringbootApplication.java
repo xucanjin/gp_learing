7a8d564aa181a6f718299c0eea7bb6a31276b54f
19749 <canjin.xu@hand-china.com>
2020-02-24 12:30:03 +0800

    11

---
+++ gpwork/src/main/java/com/xu/SpringbootApplication.java
@@ -0,0 +1,17 @@
+package com.xu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+*
+* @SpringBootApplication  表示这是一个SpringBoot应用
+**/
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
